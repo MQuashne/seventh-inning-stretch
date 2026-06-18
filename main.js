19e5c7a8eee9a88ee789f1281415e8c0efa3d909
MQuashne <mquashne@gmail.com>
2026-06-18 15:33:29 -0400

    update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import {players} from './model/players.js'
 import {maxValue} from './control/rollTest.js'
-import DiceBox from "@3d-dice/dice-box"
+import DiceBox from "https://unpkg.com/@3d-dice/dice-box@1.1.3/dist/dice-box.es.min.js";
 
 console.log("running")
 let result = players[8].bat([1,2,3,4]);
