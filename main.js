4e914775c09f215c8301295698861be8948c3495
MQuashne <57578781+MQuashne@users.noreply.github.com>
2026-06-18 18:32:44 +0000

    installed dice

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 import {players} from './model/players.js'
 import {maxValue} from './control/rollTest.js'
+import DiceBox from "@3d-dice/dice-box"
 
+console.log("running")
 let result = players[8].bat([1,2,3,4]);
 const resultDiv=document.getElementById('result');
 const nameDiv=document.getElementById('name');
 nameDiv.textContent=players[8].name;
 resultDiv.textContent=result;
+
