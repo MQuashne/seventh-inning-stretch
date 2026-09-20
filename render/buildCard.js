1bc554c3f3e28d34c060ee9a4e8e4bb705defc68
MQuashne <mquashne@gmail.com>
2026-09-20 12:36:22 -0400

    update buildCard.js

--- render/buildCard.js
+++ render/buildCard.js
@@ -14,6 +14,7 @@ function $sao(element, attributes) {
   });
 }
 
+
 const $sa = (svg, att, val) => svg.setAttribute(att, val);
 const $ne = (el) => {
   const newEl = document.createElementNS('http://www.w3.org/2000/svg', el);
